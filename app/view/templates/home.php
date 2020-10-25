c1cd7cd51285a0a370494a3ccab0b6f5a51eebb8
Aleks Devmcee <info@devmc.ee>
2020-10-25 19:21:43 +0200

    styling: frontpage

--- app/view/templates/home.php
+++ app/view/templates/home.php
@@ -9,14 +9,24 @@
     <title>Maze Solver</title>
 </head>
 <style>
-    .amountSelect select{
+    .amountSelect select {
         padding: 10px;
+    }
+    .container, .header{
+        max-width: 960px;
+        width: 100%;
+        margin: 0 auto
+    }
+    .header{
+        text-align: center;
+        padding: 20px;
     }
     .lds-dual-ring {
         display: inline-block;
         width: 80px;
         height: 80px;
     }
+
     .lds-dual-ring:after {
         content: " ";
         display: block;
@@ -28,6 +38,7 @@
         border-color: #000 transparent #000 transparent;
         animation: lds-dual-ring 1.2s linear infinite;
     }
+
     @keyframes lds-dual-ring {
         0% {
             transform: rotate(0deg);
@@ -45,25 +56,30 @@
         cursor: pointer;
     }
 
-    #selectMazeSizeForm, .container,.mazeSolverResultContainer {
+    #selectMazeSizeForm, .container, .mazeSolverResultContainer {
         padding: 10px;
         display: flex;
 
     }
-    .mazeSolverResultContainer{
+
+    .mazeSolverResultContainer {
         flex-direction: column;
         padding: 10px 20px;
     }
-    .mazeSolverResultContainer button{
+
+    .mazeSolverResultContainer button {
         padding: 20px;
     }
-    .amountSelect{
+
+    .amountSelect {
         margin-right: 10px;
     }
-    .route-marker{
+
+    .route-marker {
         background-color: greenyellow;
     }
-    #mazeSubmitBtn{
+
+    #mazeSubmitBtn {
         padding: 10px;
     }
 </style>
@@ -74,47 +90,50 @@
  */
 
 ?>
-<h1><?php
-    echo $context['title']; ?></h1>
+<div class="header">
+    <h1><?php
+        echo $context['title']; ?></h1>
+    <a href="https://github.com/devmc-ee/maze-resolver.git">GitHub</a>
+</div>
 <hr>
 <div class="container">
-<div class="mazeCreateForm">
+    <div class="mazeCreateForm">
 
-<form method="post" action="ajax.php" id="selectMazeSizeForm">
-    <div class="amountSelect">
-        <label for="rowsAmount">Rows:</label>
-        <select name="rowsAmount" id="rowsAmount" >
-            <option value=""></option>
+        <form method="post" action="ajax.php" id="selectMazeSizeForm">
+            <div class="amountSelect">
+                <label for="rowsAmount">Rows:</label>
+                <select name="rowsAmount" id="rowsAmount">
+                    <option value=""></option>
 
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
 
-        </select>
+                </select>
+            </div>
+            <div class="amountSelect">
+                <label for="columnsAmountSelect">Columns:</label>
+                <select name="columnsAmount" id="columnsAmount" disabled="disabled">
+                    <option value=""></option>
+
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+
+                </select>
+            </div>
+
+            <div class="submitBtn">
+                <button id="mazeSubmitBtn" type="submit" disabled>Find Optimal Routes</button>
+            </div>
+        </form>
+
+        <div class="mazeTableContainer"></div>
     </div>
-    <div class="amountSelect">
-        <label for="columnsAmountSelect">Columns:</label>
-        <select name="columnsAmount" id="columnsAmount" disabled="disabled">
-            <option value=""></option>
-
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-
-        </select>
-    </div>
-
-    <div class="submitBtn">
-        <button id="mazeSubmitBtn" type="submit" disabled>Find Optimal Routes</button>
-    </div>
-</form>
-
-<div class="mazeTableContainer"></div>
-</div>
     <div class="mazeSolverResultContainer">
         Select number of rows and columns, and set walls. Then sumbit.
     </div>
@@ -127,7 +146,8 @@
         ajaxurl: "//<?php echo $_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']); ?>/app/ajax.php"
     }
 </script>
-<script src="//<?php echo $_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']); ?>/assets/js/scripts.js"></script>
+<script src="//<?php
+echo $_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']); ?>/assets/js/scripts.js"></script>
 
 </body>
 </html>
